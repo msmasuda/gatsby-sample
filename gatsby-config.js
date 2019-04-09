42511a9428b667dcf055d2aed48194ec31e86161
Shinichi Masuda <shinichi.masuda@gu3.co.jp>
2019-04-09 17:05:02 +0900

    add source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `はじめてのGraphQL`,
+    description: `GraphQLを初めて使ってみます。`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -30,5 +30,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-source-hacker-news`,
   ],
 }
